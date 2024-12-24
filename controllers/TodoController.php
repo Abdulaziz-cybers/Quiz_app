5a3a2935d4f26f0dff9de15012a8c464ea41ad04
Abdulaziz <abdulazizsalihov08@gmail.com>
2024-12-24 18:37:25 +0500

    Added Router class and its' methods are static

---
+++ controllers/TodoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controllers;
+
+class TodoController
+{
+    public function show ()
+    {
+        echo "Show string from TodoController";
+    }
+}
---
+++ src/Router.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App;
+
+class Router {
+    public string|array|int|null|false $currentRoute;
+    public function __construct () {
+        $this->currentRoute = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+    public static function getRoute (): false|array|int|string|null
+    {
+        return (new static())->currentRoute;
+    }
+    public static function getResource ($route): false|string
+    {
+        $resourceIndex = mb_stripos($route, '{id}');
+        if (!$resourceIndex){
+            return false;
+        }
+        $resourceValue = substr(self::getRoute(), $resourceIndex);
+        if($limit = mb_stripos($resourceValue, '/')){
+            return substr($resourceValue, 0, $limit);
+        }
+        return $resourceValue ?: false;
+    }
+    public static function get ($route, $callback): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            self::extracted($route, $callback);
+        }
+    }
+    public static function post ($route, $callback): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            self::extracted($route, $callback);
+        }
+    }
+    public static function putApi ($route, $callback): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+            self::extracted($route, $callback);
+        }
+    }
+
+    public static function put ($route, $callback): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['_method']) && strtoupper($_POST['_method']) === 'PUT') {
+            self::extracted($route, $callback);
+        }
+    }
+    public static function delete ($route, $callback): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+            self::extracted($route, $callback);
+        }
+    }
+    public static function extracted($route, $callback): void
+    {
+        if (gettype($callback) === 'array') {
+            $resourceValue = self::getResource($route);
+            if ($resourceValue) {
+                $resourceRoute = str_replace('{id}', $resourceValue, $route);
+                if ($resourceRoute == self::getRoute()) {
+                    (new $callback[0])->{$callback[1]}();
+                    exit();
+                }
+            }
+            if ($route == self::getRoute()) {
+                (new $callback[0])->{$callback[1]}();
+                exit();
+            }
+        }
+        $resourceValue = self::getResource($route);
+        if ($resourceValue) {
+            $resourceRoute = str_replace('{id}', $resourceValue, $route);
+            if ($resourceRoute == self::getRoute()) {
+                $callback($resourceValue);
+                exit();
+            }
+        }
+        if ($route == self::getRoute()) {
+            $callback();
+            exit();
+        }
+    }
+    public static function isApiCall(): bool{
+        return mb_stripos(self::getRoute(), '/api') === 0;
+    }
+    public static function isTelegram(): bool{
+        return mb_stripos(self::getRoute(), '/telegram') === 0;
+    }
+}
