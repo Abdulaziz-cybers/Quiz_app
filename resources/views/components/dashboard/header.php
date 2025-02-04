b6f60a84f7d84f6a13835bbb7d9633a40a88c74b
Abdulaziz <abdulazizsalihov08@gmail.com>
2025-02-04 19:13:26 +0500

    bootsrap updated

--- resources/views/components/dashboard/header.php
+++ resources/views/components/dashboard/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Quizzes</title>
-    <script src="<?php echo assets('/css/output.css')?>"></script>
+    <link rel="stylesheet" href="<?php echo assets('/css/output.css')?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></link>
     <script src="<?php echo assets('/js/dashboard/getUserInfo.js')?>"></script>
 </head>
 <body class="bg-gray-100">
--- resources/views/components/main/header.php
+++ resources/views/components/main/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz App</title>
-    <script src="<?php echo assets('/css/output.css')?>"></script>
+    <link rel="stylesheet" href="<?php echo assets('/css/output.css')?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body class="bg-gradient-to-b from-gray-50 to-gray-100">
